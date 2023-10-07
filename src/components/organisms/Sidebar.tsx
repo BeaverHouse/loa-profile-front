c398ad1a1c11eea5c0cd7d380481e0cc5d4e7c42
LHU <haulrest@naver.com>
2023-10-08 01:57:18 +0900

    url update

--- src/components/organisms/Sidebar.tsx
+++ src/components/organisms/Sidebar.tsx
@@ -15,72 +15,72 @@ function Sidebar() {
     const { isDark, toggleDark } = useContext(LoaContext);
 
     const endpoints = [
-        {name: "프로필", path: "/"},
-        {name: "가토 수익 계산", path: "/calc"}
+        { name: "프로필", path: "/" },
+        { name: "가토 수익 계산", path: "/calc" }
     ]
 
     return (
-      <>
-        <div 
-            style={{
-                display: "flex", 
-                justifyContent: "center", 
-                padding: "10px 20px 10px 20px", 
-                backgroundColor: "#4169e1",
-                position: 'fixed',
-                top: 0,
-                left: 0,
-                right: 0,
-                zIndex: 99
-            }}
-        >
-            <Button 
-                style={{backgroundColor: "#2c3e50", borderColor: "#2c3e50"}}
-                type="primary"
-                shape='circle'
-                size='large'
-                icon={<MenuOutlined/>} 
-                onClick={() => setVisible(true)}
-            />            
-            <div style={{
-                flexGrow: 1, 
-                maxWidth: 1300, 
-                fontSize: 20, 
-                fontWeight: 600, 
-                lineHeight: "40px",
-                textAlign: "center"
-            }}>
-                <Link to="/" style={{color: "white"}}>
-                    Loa Profile
-                </Link>
-            </div>
-        </div>
-        <Drawer 
-            title="Menu"
-            width={250}
-            placement="left" 
-            onClose={() => setVisible(false)} 
-            open={visible} 
-            bodyStyle={{display: 'flex', flexDirection: 'column'}}
-        >
-            <div style={{flexGrow: 1, display: 'flex', flexDirection: "column"}}>
-                {endpoints.map((end, index) => (
-                    <Link key={index} to={end.path} onClick={() => setVisible(false)}>
-                        <MidText type={path!==end.path ? "secondary" : "success"}>{end.name}</MidText>                        
+        <>
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    padding: "10px 20px 10px 20px",
+                    backgroundColor: "#4169e1",
+                    position: 'fixed',
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    zIndex: 99
+                }}
+            >
+                <Button
+                    style={{ backgroundColor: "#2c3e50", borderColor: "#2c3e50" }}
+                    type="primary"
+                    shape='circle'
+                    size='large'
+                    icon={<MenuOutlined />}
+                    onClick={() => setVisible(true)}
+                />
+                <div style={{
+                    flexGrow: 1,
+                    maxWidth: 1300,
+                    fontSize: 20,
+                    fontWeight: 600,
+                    lineHeight: "40px",
+                    textAlign: "center"
+                }}>
+                    <Link to="/" style={{ color: "white" }}>
+                        Loa Profile
                     </Link>
-                ))}
-                <br/>
-                <Typography.Link href="https://github.com/BeaverHouse/loa-check-electron/releases" rel="noreferrer" target="_blank" 
-                    style={{fontSize: "13px"}} type="success" strong>
-                    클립보드 군장검사 (Windows)
-                </Typography.Link>
+                </div>
             </div>
-            <div>
-                <Switch checked={isDark} onChange={toggleDark}/>
-                <MidText>&nbsp;&nbsp;다크 모드</MidText>
-            </div>
-        </Drawer>
-      </>
+            <Drawer
+                title="Menu"
+                width={250}
+                placement="left"
+                onClose={() => setVisible(false)}
+                open={visible}
+                bodyStyle={{ display: 'flex', flexDirection: 'column' }}
+            >
+                <div style={{ flexGrow: 1, display: 'flex', flexDirection: "column" }}>
+                    {endpoints.map((end, index) => (
+                        <Link key={index} to={end.path} onClick={() => setVisible(false)}>
+                            <MidText type={path !== end.path ? "secondary" : "success"}>{end.name}</MidText>
+                        </Link>
+                    ))}
+                    <br />
+                    <Typography.Link href="https://github.com/BeaverHouse/loa-profile-electron/releases" rel="noreferrer" target="_blank"
+                        style={{ fontSize: "13px" }} type="success" strong>
+                        클립보드 군장검사 (Windows)
+                    </Typography.Link>
+                </div>
+                <div>
+                    <Switch checked={isDark} onChange={toggleDark} />
+                    <MidText>&nbsp;&nbsp;다크 모드</MidText>
+                </div>
+            </Drawer>
+        </>
     );
 }
 
